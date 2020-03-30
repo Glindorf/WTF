5fe5e747731ff1c761106838321decbcb6716412
Madeleine Glindorf <mglindorf@gmail.com>
2020-03-30 13:30:07 +0200

    Initial commit

---
+++ InsertCodeHere.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace WTF
+{
+    public class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+
+            MysticNumbers newNumber = new MysticNumbers();
+
+            newNumber.ThreeNumbers(7, 2, 3);
+            Console.WriteLine($"The result is {newNumber}");
+
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
